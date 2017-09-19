c1ead55840111baf286ba7820975280ef78113ab
Vincent Boiteau-Robert <vboiteau94@gmail.com>
2017-09-19 13:08:08 -0400

    Fix the command call to avoid using bash in bash BASHCEPTION

--- workspace/lab1/RunCmd.c
+++ workspace/lab1/RunCmd.c
@@ -9,30 +9,43 @@
 #include <sys/time.h>
 #include <time.h>
 
-int main(int argc , const char **argv) {
+int main(int argc , char **argv) {
+    // Variable initiation
     struct rusage usage;
     pid_t pid;
-    char cmd[256];
+    char cmdExecFile[32];
+
+    /**
+     * Add /bin/ before command
+     * TODO: consider using bash command which 
+     **/
+    sprintf(cmdExecFile, "/bin/%s", argv[1]);
+    argv[1] = cmdExecFile;
+
     switch(pid = fork()) {
         case -1:
             perror("Fork failed");
             break;
         case 0:
-            for (int i = 1; i < argc; i++) {
-                strcat(cmd, argv[i]);
-                strcat(cmd, " ");
-            }
-
-            char* passedCmd[] = {"sh", "-c", cmd, NULL};
-
-            execvp(passedCmd[0], passedCmd);
+            execvp(argv[1], argv+1);
             break;
         default:
             wait(NULL);
             getrusage(RUSAGE_SELF, &usage);
-            printf("\nwall time:\t%ld seconds\t%d microseconds\ncpu time:\t%ld seconds\t%d microseconds\nContext switches:\t%ld involuntary\t%ld voluntary\nPage fault:\t%ld\nPage reclaims:\t%ld\n\n", usage.ru_stime.tv_sec, usage.ru_stime.tv_usec, usage.ru_utime.tv_sec, usage.ru_utime.tv_usec, usage.ru_nivcsw, usage.ru_nvcsw, usage.ru_majflt, usage.ru_minflt);
+            /* info about rusage found in man 2 getrusage page */
+            printf(
+                    "\nsystem time:\t%ld seconds\t%d microseconds\nuser time:\t%ld seconds\t%d microseconds\nContext switches:\t%ld involuntary\t%ld voluntary\nPage fault:\t%ld\nPage reclaims:\t%ld\n\n",
+                    usage.ru_stime.tv_sec, // system seconds
+                    usage.ru_stime.tv_usec, // system microseconds
+                    usage.ru_utime.tv_sec, // user seconds
+                    usage.ru_utime.tv_usec, // user microseconds
+                    usage.ru_nivcsw, // involuntary interruption
+                    usage.ru_nvcsw, // voluntary interruption
+                    usage.ru_majflt, // page faults
+                    usage.ru_minflt // page reclaims
+                  );
             break;
-
     }
+
     return 0;
 }
